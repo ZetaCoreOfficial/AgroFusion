8d3007aa08b3c2612e465c9fe116cb4ee5766b72
JosuePadillaUnivalle <josue.padilla@univalle.edu.co>
2026-09-24 18:24:05 -0400

    fix: use supplied photos for pest control and fertilization references

--- app/Support/TrazabilidadFotoReferencia.php
+++ app/Support/TrazabilidadFotoReferencia.php
@@ -10,8 +10,8 @@ final class TrazabilidadFotoReferencia
         $path = parse_url((string) $url, PHP_URL_PATH) ?: '';
         $references = [
             'moF3BJtBeYRRkQN0P9duOdQixpHd3gIsyTI5CwKK.png' => 'images/insumos/catalogo-20260922/insumo-145.jpg',
-            'tfdKSrICuPC2XuwVtKrm2qcxQlHlUEj4qwbk6oL2.jpg' => 'images/insumos/catalogo-20260922/insumo-19.jpg',
-            'Gyc7JzFEayHXQKoehhqCFgUPZNQqSmldFcFF3dD7.jpg' => 'images/insumos/catalogo-20260922/insumo-59.jpg',
+            'tfdKSrICuPC2XuwVtKrm2qcxQlHlUEj4qwbk6oL2.jpg' => 'images/trazabilidad/control-plagas-referencia.jpeg',
+            'Gyc7JzFEayHXQKoehhqCFgUPZNQqSmldFcFF3dD7.jpg' => 'images/trazabilidad/fertilizacion-referencia.jpeg',
             '7DnWy2CJPfzQQEVA6ygRl9xxvE5gOfjZyuqQb76u.png' => 'images/trazabilidad/riego-referencia.jpg',
             'Ykn09WBiFahNCo8k7wwOBcbfL7zzKiPaADgDT0cE.jpg' => 'images/insumos/catalogo-20260922/insumo-148.jpg',
         ];
